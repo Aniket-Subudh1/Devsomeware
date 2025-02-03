d2bf5b90ca8c610b154a3eaddcd34fc796826de8
Aniket-Subudh1 <aniketsubudhi00@gmail.com>
2025-02-03 21:38:30 +0530

    Issue fixed

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    domains: ['res.cloudinary.com'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     remotePatterns: [
       {
         protocol: 'https',
@@ -12,5 +15,4 @@ const nextConfig = {
     ],
   },
 }
-
 module.exports = nextConfig;
